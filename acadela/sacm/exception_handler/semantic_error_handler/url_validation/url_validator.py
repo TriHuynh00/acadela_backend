7b03347cdde91d67fd70726a41599d55bbc6161e
Tri Huynh <tri.huynh@tum.de>
2023-09-09 11:15:44 +0200

    Finalize url validation and expression of round function

--- acadela/sacm/exception_handler/semantic_error_handler/url_validation/url_validator.py
+++ acadela/sacm/exception_handler/semantic_error_handler/url_validation/url_validator.py
@@ -77,11 +77,11 @@ def url_validator(case_object_tree):
                     missing_hook = hook
                     break
             if missing_hook:
-                line_number_text = f"at line {str(missing_hook.lineNumber[0])} and column {str(missing_hook.lineNumber[1])}"
+                line_number_text = f"at line {str(missing_hook.lineNumber[0])} " #and column {str(missing_hook.lineNumber[1])}"
                 if method_missing:
                     raise Exception(f"The URL {missing_hook.url} {line_number_text} does not accept the HTTP method "
                                     f"{missing_hook.method}.  Allowed methods: {allowed_methods}."
-                                    f"Please further check the trusted sources list for the permitted methods\n ")
+                                    f"Please further check the trusted sources list for the permitted methods.\n ")
                 else:
                     raise Exception(
                         f"The URL {missing_hook.url} {line_number_text} "
